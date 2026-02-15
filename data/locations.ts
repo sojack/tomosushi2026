af6ba28e42255773d870162d455cf3d6b4e0b464
Jack So <sojack@gmail.com>
2026-02-15 17:14:23 -0500

    accessibility fixes

--- data/locations.ts
+++ data/locations.ts
@@ -16,7 +16,7 @@ export const locations: Location[] = [
     // To get this URL: Google Maps > search restaurant > Share > Embed a map > copy the src from the iframe
     mapEmbedUrl:
       'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2876.7594072093716!2d-79.43688722305396!3d43.86081517109306!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882b2ba5ba75ea71%3A0xfaf0ad0765dd4273!2sTomo%20Sushi%20Richmond%20Hill!5e0!3m2!1sen!2sca!4v1771171613000!5m2!1sen!2sca',
-    instagramUrl: 'https://www.instagram.com/tomosushi_rh/',
+    instagramUrl: 'https://www.instagram.com/tomosushi_richmondhill/',
   },
   {
     id: 'mississauga',
@@ -33,7 +33,7 @@ export const locations: Location[] = [
     // To get this URL: Google Maps > search restaurant > Share > Embed a map > copy the src from the iframe
     mapEmbedUrl:
       'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5781.661894936106!2d-79.66787942306573!3d43.56840557110634!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882b410d77778401%3A0xbe83b1fc28f68952!2sTomo%20Sushi%20Mississauga!5e0!3m2!1sen!2sca!4v1771172830497!5m2!1sen!2sca',
-    instagramUrl: 'https://www.instagram.com/tomosushi_mg/',
+    instagramUrl: 'https://www.instagram.com/tomosushi_canada/',
   },
 ];
 
